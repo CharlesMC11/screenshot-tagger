28114e2bb4786780b94a0a59b902a7bc5c1dc64d
Charles Mesa Cayobit <charlesmesacayobit@gmail.com>
2026-01-31 01:06:38 -0800

    Add padding

--- src/functions/sst-log.zsh
+++ src/functions/sst-log.zsh
@@ -3,5 +3,5 @@
 sst-log() {
   readonly level=${(U)1}; shift
 
-  print -P -u $log_fd -f "[%s]\t[%s]\t[%s]\t%s\n" -- "%D{%F %T}" "${SERVICE_NAME}:$$" "$level" "$*"
+  print -P -u $log_fd -f "[%s]\t[%-8s]\t[%-5s]\t%s\n" -- "%D{%F %T}" "${SERVICE_NAME}:$$" "$level" "$*"
 }
